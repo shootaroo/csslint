2275e0d0fca673cbefb0fbc5dfca38b78d2f1c55
shootaroo <shotaro.tsubouchi@gmail.com>
2013-08-08 15:03:24 +0900

    Add `\t` and `\n`

--- tests/formatters/checkstyle-xml.js
+++ tests/formatters/checkstyle-xml.js
@@ -9,7 +9,7 @@
 
         "File with no problems should say so": function(){
             var result = { messages: [], stats: [] },
-                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?><checkstyle></checkstyle>";
+                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle>\n</checkstyle>";
             Assert.areEqual(expected, CSSLint.format(result, "FILE", "checkstyle-xml"));
         },
 
@@ -18,10 +18,10 @@
                      { type: "warning", line: 1, col: 1, message: "BOGUS", evidence: "ALSO BOGUS", rule: { id: "a-rule", name: "A Rule"} },
                      { type: "error", line: 2, col: 1, message: "BOGUS", evidence: "ALSO BOGUS", rule: { id: "some-other-rule", name: "Some Other Rule"} }
                 ], stats: [] },
-                file = "<file name=\"FILE\">",
-                error1 = "<error line=\"1\" column=\"1\" severity=\"warning\" message=\"BOGUS\" source=\"net.csslint.a-rule\"/>",
-                error2 = "<error line=\"2\" column=\"1\" severity=\"error\" message=\"BOGUS\" source=\"net.csslint.some-other-rule\"/>",
-                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?><checkstyle>" + file + error1 + error2 + "</file></checkstyle>",
+                file = "\t<file name=\"FILE\">\n",
+                error1 = "\t\t<error line=\"1\" column=\"1\" severity=\"warning\" message=\"BOGUS\" source=\"net.csslint.a-rule\"/>\n",
+                error2 = "\t\t<error line=\"2\" column=\"1\" severity=\"error\" message=\"BOGUS\" source=\"net.csslint.some-other-rule\"/>\n",
+                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle>\n" + file + error1 + error2 + "\t</file>\n</checkstyle>",
                 actual = CSSLint.format(result, "FILE", "checkstyle-xml");
             Assert.areEqual(expected, actual);
         },
@@ -32,10 +32,10 @@
                      { type: "warning", line: 1, col: 1, message: specialCharsSting, evidence: "ALSO BOGUS", rule: [] },
                      { type: "error", line: 2, col: 1, message: specialCharsSting, evidence: "ALSO BOGUS", rule: [] }
                 ], stats: [] },
-                file = "<file name=\"FILE\">",
-                error1 = "<error line=\"1\" column=\"1\" severity=\"warning\" message=\"sneaky, &quot;sneaky&quot;, &lt;sneaky&gt;, sneak &amp; sneaky\" source=\"\"/>",
-                error2 = "<error line=\"2\" column=\"1\" severity=\"error\" message=\"sneaky, &quot;sneaky&quot;, &lt;sneaky&gt;, sneak &amp; sneaky\" source=\"\"/>",
-                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?><checkstyle>" + file + error1 + error2 + "</file></checkstyle>",
+                file = "\t<file name=\"FILE\">\n",
+                error1 = "\t\t<error line=\"1\" column=\"1\" severity=\"warning\" message=\"sneaky, &quot;sneaky&quot;, &lt;sneaky&gt;, sneak &amp; sneaky\" source=\"\"/>\n",
+                error2 = "\t\t<error line=\"2\" column=\"1\" severity=\"error\" message=\"sneaky, &quot;sneaky&quot;, &lt;sneaky&gt;, sneak &amp; sneaky\" source=\"\"/>\n",
+                expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle>\n" + file + error1 + error2 + "\t</file>\n</checkstyle>",
                 actual = CSSLint.format(result, "FILE", "checkstyle-xml");
             Assert.areEqual(expected, actual);
         }
